fd72c450de573b0b06abbb2aa183ac41e08da2b2
Simon Onyszczak Hansen <simo401t@zbc.dk>
2022-08-09 12:19:13 +0200

    BMI

--- BMICalculator/Program.cs
+++ BMICalculator/Program.cs
@@ -14,23 +14,23 @@
             double bmi = (weight / (height * height));
 
             //checks the users bmi
-            if (bmi >= 16 || bmi < 18.5)
+            if (bmi >= 16 && bmi < 18.5)
             {
                 Console.WriteLine("Kropsvægt underskud");
             }
-            else if (bmi >= 18.5 || bmi <24)
+            else if (bmi >= 18.5 && bmi <24)
             {
                 Console.WriteLine("Normal");
             }
-            else if (bmi >= 24 || bmi < 30)
+            else if (bmi >= 24 && bmi < 30)
             {
                 Console.WriteLine("Overvægtig");
             }
-            else if (bmi >= 30 || bmi < 35)
+            else if (bmi >= 30 && bmi < 35)
             {
                 Console.WriteLine("Første grads overvægtig");
             }
-            else if (bmi >= 35 || bmi < 40)
+            else if (bmi >= 35 && bmi < 40)
             {
                 Console.WriteLine("Anden grads overvægtig");
             }

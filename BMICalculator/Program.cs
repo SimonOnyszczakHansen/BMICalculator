ea5ec0fa9cce6f88884a347d687b5157fb0a7be0
zbcsioh <zbcsioh@ZBC-S-985Y0>
2022-08-09 10:20:12 +0200

    Add project files.

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace BMICalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //We collect the users information
+            Console.WriteLine("Indtast din vægt i kg");
+            double weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Indtast din højde i m");
+            double height = double.Parse(Console.ReadLine());
+
+            //We calculate the bmi
+            double bmi = (weight / (height * height));
+
+            //checks the users bmi
+            if (bmi >= 16 || bmi < 18.5)
+            {
+                Console.WriteLine("Kropsvægt underskud");
+            }
+            else if (bmi >= 18.5 || bmi <24)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (bmi >= 24 || bmi < 30)
+            {
+                Console.WriteLine("Overvægtig");
+            }
+            else if (bmi >= 30 || bmi < 35)
+            {
+                Console.WriteLine("Første grads overvægtig");
+            }
+            else if (bmi >= 35 || bmi < 40)
+            {
+                Console.WriteLine("Anden grads overvægtig");
+            }
+            else
+            {
+                Console.WriteLine("Tredje grads overvægtig");
+            }
+            Console.Read();
+        }
+    }
+}
